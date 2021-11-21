6a407be094dab16a48eeb71647ad5e123f8e711b
Greg "GothAck" Miell <greg@gothack.ninja>
2021-11-21 15:12:30 +0000

    More benches, twiddle some knobs

--- benches/benches.rs
+++ benches/benches.rs
@@ -10,8 +10,8 @@ use tokio::{
 };
 
 const PAYLOAD_SIZE: usize = 1024 * 1024;
-const SEND_ROUND: usize = 1024 * 1024;
-const HANDSHAKE_ROUND: usize = 1024 * 1024;
+const SEND_ROUND: usize = 16 * 1024;
+const HANDSHAKE_ROUND: usize = 16 * 1024;
 
 async fn get_tcp_stream_pair() -> (TcpStream, TcpStream) {
     let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
@@ -55,47 +55,56 @@ async fn get_mux_stream_pair() -> (
 }
 
 async fn tcp_throughput() {
-    tokio::task::block_in_place(|| {
-        futures::executor::block_on(async {
-            let (mut stream0, mut stream1) = get_tcp_stream_pair().await;
+    let (mut stream0, mut stream1) = get_tcp_stream_pair().await;
 
-            tokio::spawn(async move {
-                let mut buf: Vec<u8> = vec![];
-                buf.resize(PAYLOAD_SIZE, 0);
-                for _ in 0..SEND_ROUND {
-                    stream0.write_all(&buf).await.unwrap();
-                }
-            });
-
-            let mut buf: Vec<u8> = vec![];
-            buf.resize(PAYLOAD_SIZE, 0);
-            for _ in 0..SEND_ROUND {
-                stream1.read_exact(&mut buf).await.unwrap();
-            }
-        })
+    tokio::spawn(async move {
+        let mut buf: Vec<u8> = vec![];
+        buf.resize(PAYLOAD_SIZE, 0);
+        for _ in 0..SEND_ROUND {
+            stream0.write_all(&buf).await.unwrap();
+        }
     });
+
+    let mut buf: Vec<u8> = vec![];
+    buf.resize(PAYLOAD_SIZE, 0);
+    for _ in 0..SEND_ROUND {
+        stream1.read_exact(&mut buf).await.unwrap();
+    }
 }
 
 async fn mux_throughput() {
-    tokio::task::block_in_place(|| {
-        futures::executor::block_on(async {
-            let (_mux0, _mux1, mut stream0, mut stream1) = get_mux_stream_pair().await;
+    let (_mux0, _mux1, mut stream0, mut stream1) = get_mux_stream_pair().await;
 
-            tokio::spawn(async move {
-                let mut buf: Vec<u8> = vec![];
-                buf.resize(PAYLOAD_SIZE, 0);
-                for _ in 0..SEND_ROUND {
-                    stream0.write_all(&buf).await.unwrap();
-                }
-            });
-
-            let mut buf: Vec<u8> = vec![];
-            buf.resize(PAYLOAD_SIZE, 0);
-            for _ in 0..SEND_ROUND {
-                stream1.read_exact(&mut buf).await.unwrap();
-            }
-        })
+    tokio::spawn(async move {
+        let mut buf: Vec<u8> = vec![];
+        buf.resize(PAYLOAD_SIZE, 0);
+        for _ in 0..SEND_ROUND {
+            stream0.write_all(&buf).await.unwrap();
+        }
     });
+
+    let mut buf: Vec<u8> = vec![];
+    buf.resize(PAYLOAD_SIZE, 0);
+    for _ in 0..SEND_ROUND {
+        stream1.read_exact(&mut buf).await.unwrap();
+    }
+}
+
+async fn mux_handshake() {
+    let (stream0, stream1) = get_tcp_stream_pair().await;
+    let mux0 = StreamMultiplexor::new(stream0, Config::default());
+    let mux1 = StreamMultiplexor::new(stream1, Config::default());
+
+    for i in 0..HANDSHAKE_ROUND {
+        let listener = mux0.bind(i as u16 + 1).await.unwrap();
+        tokio::spawn(async move {
+            listener.accept().await.unwrap();
+        });
+    }
+
+    for i in 0..HANDSHAKE_ROUND {
+        mux1.connect(i as u16 + 1).await.unwrap();
+    }
 }
 
 pub fn throughput(c: &mut Criterion) {
@@ -109,7 +118,7 @@ pub fn throughput(c: &mut Criterion) {
 pub fn handshake(c: &mut Criterion) {
     let mut group = c.benchmark_group("handshake");
     group.throughput(Throughput::Elements(HANDSHAKE_ROUND as u64));
-    // group.bench_function("mux", |b| b.iter(|| mux_handshake()));
+    group.bench_function("mux", |b| b.to_async(Runtime::new().unwrap()).iter(|| mux_handshake()));
     group.finish();
 }
 
